88eba5d2a9e6f9b8c0eb6201e022df3c41a3940b
nehadurani996 <nyha996@gmail.com>
2023-07-21 16:09:02 +0200

    Done few changes

--- JavaScript_DataLayer/Recurssion.js
+++ JavaScript_DataLayer/Recurssion.js
@@ -1,3 +1,23 @@
+// Added capatilize function
+
+function capitalizeObjectKeys(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+
+  const capitalizedObj = {};
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      const capitalizedKey = key.charAt(0).toUpperCase() + key.slice(1);
+      capitalizedObj[capitalizedKey] = typeof obj[key] === 'string' ? obj[key].charAt(0).toUpperCase() + obj[key].slice(1) : capitalizeObjectKeys(obj[key]);
+    }
+  }
+
+  return capitalizedObj;
+}
+
+
+
 const schema = {
   type: 'object',
   properties: {
@@ -11,18 +31,18 @@ const schema = {
         postalCode: { type: 'string' }
       }
     }
-  },
-  required: ['id', 'name', 'address']
+  }
 };
+
 // Function to validate a data object against the schema
 function validateEventPayload(eventPayload, schema) {
-  for (var propertyName in schema.properties) {
+  for (const propertyName in schema.properties) {
     if (eventPayload.hasOwnProperty(propertyName)) {
-      var property = schema.properties[propertyName];
+      const property = schema.properties[propertyName];
 
       if (property.type === 'object' && typeof eventPayload[propertyName] === 'object' && eventPayload[propertyName] !== null) {
         // Recursively validate nested object
-        var isValidNestedObject = validateEventPayload(eventPayload[propertyName], property);
+        const isValidNestedObject = validateEventPayload(eventPayload[propertyName], property);
         if (!isValidNestedObject) {
           return false;
         }
@@ -32,10 +52,7 @@ function validateEventPayload(eventPayload, schema) {
           return false;
         }
       }
-    } else {
-      console.error('Error: Event payload is missing required property: ' + propertyName);
-      return false;
-    }
+    } 
   }
 
   return true;
@@ -43,15 +60,36 @@ function validateEventPayload(eventPayload, schema) {
 
 // Example nested JSON object
 const data = {
-  id: 123,
-  name: 'John Doe',
+  id: 123, // This is the required 'id' property
+  name: 'john Doe',
   address: {
     street: '123 Main St',
-    city: 'New York',
+    city: 'new York',
     postalCode: '10001'
   }
 };
 
-// Perform validation
-const isValid = validateEventPayload(data, schema);
-console.log('Data is valid:', isValid);
+// Perform validation and push to breadLayer
+const breadLayer = [];
+breadLayer.push = (eventPayload) => {
+  if (!eventPayload) {
+    console.error('Error: No event payload provided to push to breadLayer');
+    return null;
+  }
+
+  const capitalizedPayload = capitalizeObjectKeys(eventPayload);
+  const valid = validateEventPayload(capitalizedPayload, schema);
+  if (!valid) {
+    console.error('Error: Invalid event payload');
+    return null;
+  }
+
+  Array.prototype.push.call(breadLayer, capitalizedPayload);
+  console.log('breadLayer:', breadLayer); // Debug log to check if the data is getting pushed
+};
+
+breadLayer.push(data); // Ensure this line is executed in your environment
+
+// Add a log message to indicate the end of the script execution
+console.log('Script execution completed.');
+
